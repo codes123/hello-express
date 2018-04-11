2629113f58de361556116e84c5a5e995cdb79aba
mbila <taniambila@yahoo.co.uk>
2018-04-11 15:43:14 +0100

    first commit stept 4

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const helloWorld = require('./controllers/helloWorld');
+const createShoppingList = require('./controllers/createShoppingList')
+
+const app = express();
+
+app.get('/', helloWorld);
+app.use(bodyParser.json());
+app.post('/shopping-lists', createShoppingList);
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
